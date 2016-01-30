d84cdef54f4d3d92bdc039fd6ee762dd000839a4
Matt Eckman <matteckman@gmail.com>
2016-01-30 09:07:36 -0500

    latest

--- app/mainController.js
+++ app/mainController.js
@@ -82,34 +82,9 @@ function ($state,$rootScope,$scope, posts, api, user, $q, $aside) {
 	// =====================
 	// SIDEBAR ACCORDION(S)
 	// =====================
+
 	$scope.isCollapsed = false;
 
-	// =====================
-	// LOG IN FUNCTION
-	// =====================
-	
-
-	/*$scope.login = function(){
-console.log()
-		promise = $q.defer();
-		api.logIn(promise,$scope.loginFormFields.email,
-			$scope.loginFormFields.password).then(
-			function(result){
-				// add values to user service
-				if(user){
-					user.saveUser(result.user);
-					user.setToken(result.token);
-					// redirect to home
-					$state.go('private.home'); 
-				}else{
-					// user service not available, means this needs to fail
-					// TODO add a flash message to appear to user
-					$state.go('public.logInPage');
-				}
-			}
-		);
-
-	};*/
 
 	// =====================
 	// LOGOUT FUNCTION
